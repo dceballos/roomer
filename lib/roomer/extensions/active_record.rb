88c1640183b48e971d7c58134bae000a95f86c8d
Daniel Ceballos <danielceballos@gmail.com>
2011-08-11 17:25:34 -0700

    basic working active_record patch module

--- lib/roomer/extensions/active_record.rb
+++ lib/roomer/extensions/active_record.rb
@@ -1,28 +1,48 @@
 class ActiveRecord::Base
-  def self.table_name_prefix
-    return @@table_name_prefix unless @@table_name_prefix.blank?
-    if self.shared?
-      Roomer.shared_schema_name
-    elsif self.tenanted?
-      self.current_tenant[Roomer.tenant_schema_name_column]
-    else
-      ""
+  class << self
+    def table_name_prefix
+      return @table_name_prefix unless @table_name_prefix.blank?
+      if shared?
+        Roomer.shared_schema_name
+      elsif tenanted?
+        self.current_tenant[Roomer.tenant_schema_name_column]
+      else
+        ""
+      end
+    end
+
+    def roomer(scope)
+      case scope
+        when :shared
+          @roomer_scope = :shared
+        when :tenanted
+          @roomer_scope = :tenanted
+        else
+          raise "Invalid roomer model scope.  Choose :shared or :tenanted"
+      end
+    end
+
+    def shared?
+      @roomer_scope == :shared
+    end
+
+    def tenanted?
+      @roomer_scope == :tenanted
+    end
+
+    def current_tenant=(val)
+      key = :"#{self}_current_tenant"
+      Thread.current[key] = val
+    end
+    
+    def current_tenant
+      key = :"#{self}_current_tenant"
+      Thread.current[key] || ""
+    end
+
+    def reset_current_tenant
+      key = :"#{self}_current_tenant"
+      Thread.current[key] = nil
     end
   end
-  
-  def self.current_tenant=(val)
-    key = :"#{self}_current_tenant"
-    Thread.current[key] = val
-  end
-  
-  def self.current_tenant
-    key = :"#{self}_current_tenant"
-    Thread.current[key] || ""
-  end
-
-  def self.reset_current_tenant
-    key = :"#{self}_current_tenant"
-    Thread.current[key] = nil
-  end
 end
-
