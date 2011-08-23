5327e441fdd260ea96e6ccaecd08c2a0893a2181
Daniel Ceballos <danielceballos@gmail.com>
2011-08-23 01:46:01 -0700

    fixing schemas directory

--- lib/generators/roomer/install/templates/roomer.rb
+++ lib/generators/roomer/install/templates/roomer.rb
@@ -58,8 +58,8 @@ Roomer.setup do |config|
   config.tenanted_migrations_directory = "#{Roomer.tenanted_migrations_directory}"
 
   # Directory where schema files will be stored.
-  # This may be useful for deploying on heroku (ie: File.join("tmp", "schemas"))
-  # config.schemas_directory = File.join("db", "schemas")
+  # This may be useful for deploying on heroku (ie: File.join(Rails.root, "tmp", "schemas"))
+  # config.schemas_directory = File.join(Rails.root, "db", "schemas")
 
   # Filename to use for tenanted schema
   # config.tenanted_schema_filename = "tenanted_schema.rb"
