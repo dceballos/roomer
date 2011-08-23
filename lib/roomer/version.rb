71481176a152581cbe6afae4ac4467f459a38b38
Greg Osuri <gosuri@gmail.com>
2011-08-23 00:08:07 -0700

    updating version

--- lib/roomer/version.rb
+++ lib/roomer/version.rb
@@ -3,7 +3,7 @@ module Roomer
     unless defined? MAJOR
       MAJOR  = 0
       MINOR  = 0
-      TINY   = 1
+      TINY   = 4
       PRE    = nil
 
       STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
