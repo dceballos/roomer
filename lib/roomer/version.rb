36fdc2353b49a62f6092c2aa2b3a8e0c522d71d4
Greg Osuri <gosuri@gmail.com>
2011-10-11 14:46:42 -0700

    bumping version

--- lib/roomer/version.rb
+++ lib/roomer/version.rb
@@ -3,7 +3,7 @@ module Roomer
     unless defined? MAJOR
       MAJOR  = 0
       MINOR  = 0
-      TINY   = 9
+      TINY   = 10
       PRE    = nil
 
       STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
