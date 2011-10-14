7c010343ceb0659b7bfc584d404e015d1e896306
Greg Osuri <gosuri@gmail.com>
2011-10-14 16:23:01 -0700

    adding extra helpers

--- lib/roomer/version.rb
+++ lib/roomer/version.rb
@@ -3,7 +3,7 @@ module Roomer
     unless defined? MAJOR
       MAJOR  = 0
       MINOR  = 0
-      TINY   = 10
+      TINY   = 11
       PRE    = nil
 
       STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
