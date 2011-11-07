40199c3343db8a69319ff0a9cfa9fbad863f54da
Daniel Ceballos <danielceballos@gmail.com>
2011-11-07 14:13:45 -0800

    renaming default index name to remove schema name

--- lib/roomer/schema_dumper.rb
+++ lib/roomer/schema_dumper.rb
@@ -12,12 +12,22 @@ Roomer::Schema.define(#{define_params}) do
 HEADER
     end
 
+    def roomer_index_name(index_name)
+      sections = index_name.split(".")
+      if sections.length > 1
+        if sections[0].split("_")[0] == "index"
+          sections[0] = "index"
+        end
+      end
+      sections.join(".")
+    end
+
     def indexes(table, stream)
       if (indexes = @connection.indexes(table)).any?
         add_index_statements = indexes.map do |index|
           statement_parts = [ ('add_index ' + index.table.inspect) ]
           statement_parts << index.columns.inspect
-          statement_parts << (':name => "' + index.name + '"')
+          statement_parts << (':name => "' + roomer_index_name(index.name) + '"')
           statement_parts << ':unique => true' if index.unique
   
           index_lengths = index.lengths.compact if index.lengths.is_a?(Array)
