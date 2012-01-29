80e24d94c4b24a28ddcf56e9eac5396e9e6e2817
Daniel Ceballos <danielceballos@gmail.com>
2012-01-28 19:20:18 -0800

    cleanup

--- lib/roomer/extensions/model.rb
+++ lib/roomer/extensions/model.rb
@@ -43,12 +43,11 @@ module Roomer
           roomer_set_table_name_prefix
           reset_table_name 
           reset_column_information
-          reload_environment
+          clean_environment
         end
 
-        def reload_environment
+        def clean_environment
           ActionDispatch::Reloader.cleanup!
-          #ActionDispatch::Reloader.prepare!
         end
 
         protected
