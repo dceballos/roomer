ecf27e4d18e8fafac35aa5fdfbba405dd3b30fa9
Daniel Ceballos <danielceballos@gmail.com>
2012-01-29 22:10:00 -0800

    comparing url identifier

--- lib/roomer/utils.rb
+++ lib/roomer/utils.rb
@@ -40,7 +40,7 @@ module Roomer
     # @return [Symbol] the current tenant key in the thread
     def current_tenant=(val)
       key = :"roomer_current_tenant"
-      unless  Thread.current[key].try(:name) == val.name
+      unless  Thread.current[key].try(:url_identifier) == val.url_identifier
         Thread.current[key] = val
         ensure_tenant_model_reset
       end
