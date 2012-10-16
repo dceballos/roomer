44340f7920887aacbb22a5fb23a04868041d7393
Greg Osuri <gosuri@gmail.com>
2012-10-16 15:45:59 -0700

    Testing for Rails 3.2.8

---
+++ Appraisals
@@ -0,0 +1,11 @@
+appraise "rails3" do
+  gem "rails", "3.0.9"
+end
+
+appraise "rails31" do
+  gem "rails", "3.1.8"
+end
+
+appraise "rails32" do
+  gem "rails", "3.2.8"
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,3 @@
-
 # FIXME Remove Postgres Server Hard dependency
 
 ENV["RAILS_ENV"] = "test"
@@ -7,17 +6,5 @@ require "roomer"
 require "rails_app/config/environment"
 require "rails/test_help"
 
-require 'mocha'
-#require 'webrat'
-#Webrat.configure do |config|
-#  config.mode = :rails
-#  config.open_error_files = false
-#end
-
-# Add support to load paths so we can overwrite broken webrat setup
-#$:.unshift File.expand_path('../support', __FILE__)
-#Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-
 # For generators
 require "rails/generators/test_case"
-
