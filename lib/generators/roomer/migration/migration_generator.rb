2a13c6438e30e9089c2e948c38ea67ba60bd98d7
Wendy Kempter <wkempter@marketron.com>
2015-04-03 15:18:05 -0500

    Fixed rails generate roomer:migration for rails4

--- lib/generators/roomer/migration/migration_generator.rb
+++ lib/generators/roomer/migration/migration_generator.rb
@@ -4,7 +4,9 @@ module Roomer
   module Generators
     class MigrationGenerator < Rails::Generators::NamedBase
       include Rails::Generators::Migration
-      extend  ActiveRecord::Generators::Migration
+      # commented out for for rails 4.0 can probably be uncommented for 4.1
+      # extend  ActiveRecord::Generators::Migration
+      extend  ActiveRecord::Generators # rails 4.0 specific
       include Roomer::Helpers::GeneratorHelper
 
       source_root File.expand_path("../templates", __FILE__)
@@ -14,16 +16,26 @@ module Roomer
       class_option  :shared,      :type => :boolean, :default => false,
                     :aliases => "-s", :desc => "shared?"
 
+      # needed for rails 4.0 can probably be removed for 4.1
+      def self.next_migration_number(dirname)
+        next_migration_number = current_migration_number(dirname) + 1
+        ActiveRecord::Migration.next_migration_number(next_migration_number)
+      end
+      
       # Generates the migration
       def create_migration_file
+        puts "!!!!!!! create_migration_file called"
         set_local_assigns!
-        migration_template "migration.rb", "#{migration_dir}/roomer_#{file_name}"
+        # migration_template "migration.rb", "#{migration_dir}/roomer_#{file_name}"
+        # had to append the .rb for rails 4.0
+        migration_template "migration.rb", "#{migration_dir}/roomer_#{file_name}.rb"
       end
 
       protected
         attr_reader :migration_action
 
         def set_local_assigns!
+          puts "!!!!!!! set_local_assigns! called"
           if file_name =~ /^(add|remove)_.*_(?:to|from)_(.*)/
             @migration_action = $1
             @table_name       = $2.pluralize
